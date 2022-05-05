7618dc6eec2c15f5d82538e03be00f1d50441746
Hunter7p <diego.mendeskp@gmail.com>
2022-05-05 15:31:58 -0300

    botei o a

--- src/pags/loginpag/index.jsx
+++ src/pags/loginpag/index.jsx
@@ -39,7 +39,7 @@ const LoginPage = () => {
             </div>
 
             <div className='field'>
-                <label htmlFor='password'>Senh</label>
+                <label htmlFor='password'>Senha</label>
                 <input type='password' name='password'  id='password' value={password} onChange={(e) => setPassword(e.target.value)} />
             </div>
 
